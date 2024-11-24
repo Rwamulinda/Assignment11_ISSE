f9e9e1c7b9238da623499d86cc0973676fad409b
uwasepauline7 <puwase@andrew.cmu.edu>
2024-11-24 19:52:17 +0200

    updating et value

--- parse.c
+++ parse.c
@@ -42,7 +42,7 @@ static ExprTree assignment(CList tokens, char *errmsg, size_t errmsg_sz)
 
     if (TOK_next_type(tokens) == TOK_EQUAL)
     {
-      TOK_consume(tokens);                                    // Consume '='
+      TOK_consume(tokens);                                  // Consume '='
       ExprTree value = additive(tokens, errmsg, errmsg_sz); // Parse the right-hand side of the assignment
       if (value == NULL)
       {
@@ -56,7 +56,6 @@ static ExprTree assignment(CList tokens, char *errmsg, size_t errmsg_sz)
   // If no assignment detected, fall back to the next rule (additive)
   return additive(tokens, errmsg, errmsg_sz);
 }
-
 
 static ExprTree additive(CList tokens, char *errmsg, size_t errmsg_sz)
 {
